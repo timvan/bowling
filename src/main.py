46826610981ec5481b1791d8851e1c3afdca9254
tim <tim@mindhive.co.nz>
2025-05-25 10:22:53 +1200

    Perfect game!

--- src/main.py
+++ src/main.py
@@ -1,54 +1,68 @@
 class Game:
     def __init__(self, score_sheet: str):
-        self.score_sheet: str = score_sheet.replace(" ", "")
-        self._scores = []
+        self.score_sheet = score_sheet.split(" ")
+        self.frames: list[Frame] = []
 
-    def _calculate_symbol_score(self, i: int):
-        roll = self.score_sheet[i]
-        score = 0
+        self.calculate_score()
 
-        if roll == "-":
-            score = 0
-        elif roll.isnumeric():
-            score = int(roll)
-        elif roll == "/":
-            score = 10 - self._calculate_symbol_score(i - 1)
-        elif roll == "X":
-            score = 10
-        else:
-            raise
-        return score
-
-    def _calculate_multipliers(self, i: int):
-        roll = self.score_sheet[i]
-        multiplier = 0
-
-        if roll == "/":
-            multiplier = self._calculate_multiplier_score(i, 1)
-        elif roll == "X":
-            multiplier = self._calculate_multiplier_score(i, 2)
-        return multiplier
-
-    def _calculate_multiplier_score(self, i: int, n: int):
-        roll = self.score_sheet[i]
-        multiplier = 0
-
-        remaining_rolls = len(self.score_sheet) - i
-
-        for j in range(min(remaining_rolls, n)):
-            multiplier += self._calculate_symbol_score(i + j + 1)
-
-        return multiplier
+    def calculate_score(self):
+        for f in self.score_sheet:
+            self.frames.append(Frame())
+            for roll in f:
+                for frame in self.frames:
+                    frame.take_roll(roll)
 
     def score(self):
-        self._scores = []
-        for i in range(len(self.score_sheet)):
-            base_score = self._calculate_symbol_score(i)
-            multiplier_score = self._calculate_multipliers(i)
-            roll_score = base_score + multiplier_score
-            self._scores.append(roll_score)
+        return sum([f.score for f in self.frames])
 
-        return sum(self._scores)
+    def __repr__(self):
+        return str([f.score for f in self.frames])
+
+
+class Frame:
+    def __init__(self):
+        self._rolls = []
+        self._roll_values = []
+        self.completed = False
+
+    def take_roll(self, roll: str):
+        if self.completed:
+            return
+
+        self._rolls.append(roll)
+        self._roll_values.append(self._roll_to_value(roll))
+
+        if self.is_spare or self.is_strike:
+            if len(self._rolls) == 3:
+                self.completed = True
+        else:
+            if len(self._rolls) == 2:
+                self.completed = True
+
+    @property
+    def is_spare(self):
+        return "/" in self._rolls
+
+    @property
+    def is_strike(self):
+        return "X" in self._rolls
+
+    def _roll_to_value(self, roll: str):
+        if roll == "-":
+            return 0
+        if roll.isnumeric():
+            return int(roll)
+        if roll == "/":
+            return 10 - self._roll_values[-1]
+        if roll == "X":
+            return 10
+
+    @property
+    def score(self):
+        return sum(self._roll_values)
+
+    def __repr__(self):
+        return str(self._rolls)
 
 
 def score(score_sheet: str):
--- tests/test_bowling.py
+++ tests/test_bowling.py
@@ -1,5 +1,5 @@
 import pytest
-from src.main import score
+from src.main import Frame, score
 
 
 @pytest.mark.parametrize(
@@ -20,11 +20,35 @@ from src.main import score
         ("4/ 4- X 14 -- -- -- -- -- --", 38),
         ("4/ 4- X X X 1- -- -- -- --", 81),
         ("4/ 4- X 4/ X 1- -- -- -- --", 70),
-        ("4/ 4- X 4/ X 1- -- -- -- --", 70),
-        ("4/ 4- X 4/ X 1- -- -- -- --", 70),
-        ("X X X X X X X X X X XXX", 300),
+        ("X X X X X X X X X XXX", 300),
         ("6/ 7- -- -4 51 -- -/ 3- -- 9/X", 70),
+        ("1/ 27 X X 4/ X 3/ 52 71 0/4", 149),
+        ("1/ 27 X X 4/ X 3/ 52 71 XX4", 159),
+        ("1/ 27 X X 4/ X 3/ 52 71 X3/", 155),
     ],
 )
 def test_bowling(score_sheet, expected_score):
     assert score(score_sheet) == expected_score
+
+
+@pytest.mark.parametrize(
+    ("score_sheet,expected_score"),
+    [
+        ("--", 0),
+        ("-1", 1),
+        ("11", 2),
+        ("1/", 10),
+        ("5/", 10),
+        ("5/3", 13),
+        ("5/32", 13),
+        ("X", 10),
+        ("X5-", 15),
+        ("XXX", 30),
+        ("XXXX", 30),
+    ],
+)
+def test_frame(score_sheet, expected_score):
+    f = Frame()
+    for i in score_sheet:
+        f.take_roll(i)
+    assert f.score == expected_score
